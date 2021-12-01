ae0cc579434dbcd12d6d8bd61d5f8e876c716b93
davidcanogallardo <93164111+davidcanogallardo@users.noreply.github.com>
2021-12-01 13:00:16 +0100

    eee

--- app.js
+++ app.js
@@ -1,19 +1,119 @@
+var propsCount = 0
+//TODO depurar inputs
+//TODO no repes inputs
+//TODO avisar del formato
+//TODO funciones primera letra mayus
 $(document).ready(function () {
+    addProp()
     $("#update").click(function (e) { 
-        var form = $("#class").serializeArray()
+        var props = getProps()
+        // var form = $("#class").serializeArray()
         var str = ""
-        form.forEach(element => {
-            if (element.name == "class-name") {
-                str += "public static class "+element.value+" {\n}"
+
+        //Crear clase
+        str += "public static class "+$("#class-name")[0].value+" {\n"
+
+        //Crear atributos
+        props.forEach(e => {
+            str +="    "+ e["accesibility"] + " " + e["property-type"] + " " + e["property-name"] + ";\n"
+        });
+
+        str += "\n"
+
+        //Crear setters y getters
+        props.forEach(e => {
+            if (e["setter"] != null) {
+                str += "    public void set"+e["property-name"]+ "("+ e["property-type"] + " " +e["property-name"]+") {\n"
+                str += "        this."+e["property-name"]+" = "+e["property-name"]+";\n"
+                str += "    }\n\n"
+            }
+            if (e["getter"] != null) {
+                str += "    public "+ e["property-type"] + " get"+e["property-name"]+ "() {\n"
+                str += "        return "+e["property-name"]+";\n"
+                str+="    }\n\n"
             }
         });
-        $(".property").each((i, value) => {
-            console.log(value);
-        });
-    
+
+        //Fin
         str += "}"
-        console.log("sggs");
+
+        //Print
+        window.str = str
+        console.log(str)
         $("#result")[0].innerText = str
-        
     });
+
+    $("#add").click(function () { 
+        addProp()
+    })
+
+    $('.property-type').on('change', function(event) {
+        if (this.value == "custom") {
+            console.log(event);
+            var id = event.target.id
+            $(event.target).replaceWith(`<input type="text" name="property-type" id="`+id+`"></input>`)
+        }
+      });
 })
+
+function getProps() {
+    var props = []
+    $(".property").each((i, value) => {
+        let prop = []
+        $(value).serializeArray().forEach(element => {
+            var name = element.name
+            var value = element.value
+            prop[name] = value
+        });
+        props.push(prop)
+    });
+
+    return props
+}
+
+function addProp() {
+    var form = 
+    `
+    <hr/>
+    <form action="" method="post" class="property">
+        <label for="">Property name <input type="text" name="property-name" id=""></label>
+        <label for="">Property type 
+            <select name="property-type" class="property-type" id="type`+propsCount+`">
+                <option value="int">int</option>
+                <option value="String">String</option>
+                <option value="boolean">boolean</option>
+                <option value="char">char</option>
+                <option value="custom">custom</option>
+            </select>
+        </label>
+        <div>Accesibility</div>
+            <div>
+                <input type="radio" id="public`+propsCount+`" name="accesibility" value="public"
+                checked>
+                <label for="public`+propsCount+`">public</label> 
+            </div>
+            <div>
+                <input type="radio" id="private`+propsCount+`" name="accesibility" value="private"
+                >
+                <label for="private`+propsCount+`">private</label> 
+            </div>
+            <div>
+                <input type="radio" id="protected`+propsCount+`" name="accesibility" value="protected"
+                >
+                <label for="protected`+propsCount+`">protected</label> 
+            </div>
+        <label for="">Methods 
+            <div>
+                <input type="checkbox" id="setter`+propsCount+`" name="setter" value="setter">
+                <label for="setter`+propsCount+`">setter</label>
+            </div>
+            <div>
+                <input type="checkbox" id="getter`+propsCount+`" name="getter" value="getter">
+                <label for="getter`+propsCount+`">getter</label>
+            </div>
+        </label>
+    </form>
+    `
+    propsCount++
+    $("#content").append(form)
+}
